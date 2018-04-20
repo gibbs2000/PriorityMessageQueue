c2164453ce32796cb2e9a06e8384c09459aa99e9
srgib <gibbonss@bishopireton.org>
2018-04-19 23:51:40 -0400

    commit before i forget, theres a bug i cant crack

--- PriorityMessageQueue/src/MessagePriorityQueue.java
+++ PriorityMessageQueue/src/MessagePriorityQueue.java
@@ -12,24 +12,34 @@ public class MessagePriorityQueue {
 	public static void fillQueue() {
 		for (int i = 0; i < 5; i++) {
 			messages.add(new PriorityQueue<Message>());
-
+		}
+		for (int i = 0; i < 5; i++) {
+			waitingTimes.add(0);
 		}
 	}
 
 	public static void eventOccurs() {
 		Message msg = new Message((int) (Math.random() * 5), curArrivalTime, curMsg++);
 		messages.get(msg.getPriority()).add(msg);
+		waitingTimes.set(msg.getPriority(), waitingTimes.get(msg.getPriority()) + (curArrivalTime - msg.getArrival()));
 		curArrivalTime += 1;
 	}
 
 	public static void processMessages() {
-		for (PriorityQueue<Message> queue : messages) {
+		for (int j = 0; j < messages.size(); j++) {
+			int numMessages = 0;
+			PriorityQueue<Message> queue = messages.get(j);
 			for (int i = 0; i < queue.size(); i++) {
 				Message msg = queue.remove();
 				System.out.println(msg + " was removed at: " + curArrivalTime);
 				System.out.println("That message had a waiting time of " + (curArrivalTime - msg.getArrival()));
 				curArrivalTime += 4;
+				numMessages++;
 			}
+			System.out.println("The average waiting time of the Queue with Priority " + j + " was: "
+					+ (waitingTimes.get(j) / numMessages));
+			System.out.println();
+			System.out.println();
 		}
 
 	}
